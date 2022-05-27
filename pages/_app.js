0402a2e47641ec5b320877bc64855385f68d594c
swaaz <swaasthik.shetty07@gmail.com>
2022-05-27 22:16:37 +0530

    Fixed typo

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { ThemeProvider } from 'styled-components';
 import Script from "next/script";
+import GoogleAnalytics from '../components/GoogleAnalytics';
 
 const theme = {
   colors: {
@@ -8,28 +9,14 @@ const theme = {
     primary: 'pink'
   },
 }
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-
-      <Script strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
-        `}
-      </Script>
-
-
     <ThemeProvider theme={theme} >
       <Component {...pageProps} />
     </ThemeProvider>
+    <GoogleAnalytics />
     </>
   )
 }
